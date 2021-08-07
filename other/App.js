167aa881572bbb78a0e45da49e807993bf5f16e0
Rick Potter <36988302+RickHPotter@users.noreply.github.com>
2021-08-07 12:44:59 -0300

    Add files via upload

---
+++ other/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+
+function App() {
+  const [token, setToken] = useState();
+
+  if (!token) { return <Login setToken={ setToken } /> }
+    else
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+          <Route path="/preferences">
+            <Preferences/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ other/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+
+async function loginUser(credentials) {
+	return fetch('http://localhost:8060/login', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(credentials)
+	})
+		.then(data => data.json())
+}
+
+export default function Login( { setToken } ) {
+	const [username, setUserName] = useState();
+	const [password, setPassword] = useState();
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		const token = await loginUser({
+			username, password
+		});
+		setToken(token);
+	}
+
+	return (
+		<div className="login-wrapper">
+			<h1>Faça o Login</h1>
+			<form onSubmit= { handleSubmit } >
+				<label>
+					<p>Usuário</p>
+					<input type="text" 
+						onChange= { e => setUserName(e.target.value) } />
+				</label>
+				<label>
+					<p>Senha</p>
+					<input type="password" 
+						onChange= { e => setPassword(e.target.value) } />
+				</label>
+				<div>
+					<button type="submit">Acessar</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+Login.propTypes = { setToken: PropTypes.func.isRequired }
